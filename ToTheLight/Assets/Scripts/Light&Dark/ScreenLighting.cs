1fe92691f01615e0ec985d8bab377cffefdac7d5
PavelSergaziev <dargoth@narod.ru>
2018-08-26 23:02:31 +0300

    изменение освещения в зависимости от расстояния до лампы
    Чёрный квадрат на канвасе, и на нём скрипт, который в корутине в бесконечном цикле меняет его прозрачность в зависимости от расстояния игрока до лампы. Изменения линейные, если б знал математику, прилепил бы более красивых кривых, но пока вроде и так норм. расстояния и минимумы-максимумы потом настроим.

---
+++ ToTheLight/Assets/Scripts/Light&Dark/ScreenLighting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenLighting : MonoBehaviour {
+
+    private Transform _player;
+    private Image _darkness;
+    private WaitForSeconds _delay;
+
+    [SerializeField]
+    private Transform _mainLight;
+    [SerializeField]
+    private float _lightChangeDelay;
+    [SerializeField]
+    private float _minAlpha;
+    [SerializeField]
+    private float _maxAlpha;
+    [SerializeField]
+    private float _startingDistanceToLight;
+    [SerializeField]
+    private float _alphaCorrectionValue;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _darkness = transform.GetComponent<Image>();
+        _player = FindObjectOfType<PlayerController>().transform;
+        _delay = new WaitForSeconds(_lightChangeDelay);
+
+        StartCoroutine("ChangeLighting");
+	}
+	
+	private IEnumerator ChangeLighting()
+    {
+        while (true)
+        {
+            float targetAlpha = ((_player.position - _mainLight.position).sqrMagnitude * _alphaCorrectionValue) / _startingDistanceToLight;
+            
+            if (targetAlpha > _maxAlpha)
+                targetAlpha = _maxAlpha;
+            else if (targetAlpha < _minAlpha)
+                targetAlpha = _minAlpha;
+
+            _darkness.color = new Color(_darkness.color.r, _darkness.color.g, _darkness.color.b, targetAlpha);
+
+            yield return _delay;
+        }
+    }
+}
