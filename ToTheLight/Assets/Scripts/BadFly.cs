eb86d402a61ef37d2dedae57008e5f406de1ce2b
PavelSergaziev <dargoth@narod.ru>
2018-08-31 11:30:53 +0300

    Revert "Мердж не удался, вернул всё как было"
    This reverts commit 38c58b14820ccade569979df33579e9983febaff.

--- ToTheLight/Assets/Scripts/BadFly.cs
+++ ToTheLight/Assets/Scripts/BadFly.cs
@@ -61,6 +61,9 @@ public class BadFly : BaseEnemy
     {
         transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, Time.deltaTime * followSpeed);
     }
+<<<<<<< HEAD
+
+=======
     void OnTriggerEnter2D(Collider2D other)
     {
         var playerScript = other.GetComponent<PlayerController>();
@@ -69,6 +72,7 @@ public class BadFly : BaseEnemy
             Debug.Log("нанесен урон");
         }
     }
+>>>>>>> parent of 85c3695... Merge branch 'lamp-&-spikes'
     void SoundControl()
     {
         if (Vector3.Distance(transform.position, _player.transform.position) < soundRange)
@@ -88,5 +92,9 @@ public class BadFly : BaseEnemy
             }
         }
     }
+<<<<<<< HEAD
+
+=======
+>>>>>>> parent of 85c3695... Merge branch 'lamp-&-spikes'
 
 }
