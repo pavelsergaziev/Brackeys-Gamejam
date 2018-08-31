dbac9d0f727dae43292c07ff026e99892bfc91df
PavelSergaziev <dargoth@narod.ru>
2018-08-31 18:47:31 +0300

    credits
    сделал

--- ToTheLight/Assets/Scripts/TitleMenu/TitleMenu.cs
+++ ToTheLight/Assets/Scripts/TitleMenu/TitleMenu.cs
@@ -5,6 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class TitleMenu : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject _titlePanel;
+
+    [SerializeField]
+    private GameObject _creditsPanel;
+
+    [SerializeField]
+    private float _creditsRollingSpeed;
+
+    private bool _inCredits;
+
+    Vector3 _creditsPanelStartingPosition;
+
+    void Update()
+    {
+        if (_inCredits)
+        {
+            if (Input.GetButtonDown("Pause") || Input.GetMouseButtonDown(0))
+                ShowMainMenu();
+
+            _creditsPanel.transform.position += new Vector3(0, Time.deltaTime * _creditsRollingSpeed, 0);
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("MainGame");
@@ -14,4 +38,24 @@ public class TitleMenu : MonoBehaviour {
     {
         Application.Quit();
     }
+
+    public void ShowCredits()
+    {
+        _creditsPanelStartingPosition = _creditsPanel.transform.position;
+
+        _titlePanel.SetActive(false);
+        _creditsPanel.SetActive(true);
+        _inCredits = true;
+    }
+
+    private void ShowMainMenu()
+    {
+        _creditsPanel.SetActive(false);
+        _creditsPanel.transform.position = _creditsPanelStartingPosition;
+        _titlePanel.SetActive(true);
+        _inCredits = false;
+    }
+
+
+
 }
