5a547094a28ab634ca365c9748c591805891ed53
PavelSergaziev <dargoth@narod.ru>
2018-08-31 11:39:19 +0300

    скрипт мухи правка после мерджа
    сабж

--- ToTheLight/Assets/Scripts/BadFly.cs
+++ ToTheLight/Assets/Scripts/BadFly.cs
@@ -61,18 +61,7 @@ public class BadFly : BaseEnemy
     {
         transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, Time.deltaTime * followSpeed);
     }
-<<<<<<< HEAD
 
-=======
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        var playerScript = other.GetComponent<PlayerController>();
-        if (playerScript != null)
-        {
-            Debug.Log("нанесен урон");
-        }
-    }
->>>>>>> parent of 85c3695... Merge branch 'lamp-&-spikes'
     void SoundControl()
     {
         if (Vector3.Distance(transform.position, _player.transform.position) < soundRange)
@@ -92,9 +81,6 @@ public class BadFly : BaseEnemy
             }
         }
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 85c3695... Merge branch 'lamp-&-spikes'
 
 }
